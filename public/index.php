2f25de1a6a971d56e01e70ef9c95dda6ab35b963
Supnovik <nikita_malytin@dotwrk.com>
2021-10-29 17:42:25 +0700

    main page header

--- public/index.php
+++ public/index.php
@@ -1,1 +1,26 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if ('/' === $uri) {
+        require 'src/Pages/Main/main.php';
+    } elseif ('/show' === $uri ) {
+        require 'src/Pages/Main/main.php';
+    } else {
+        echo '<html><body><h1>Page Not Found</h1></body></html>';
+        echo $uri;
+    }?>
+</body>
+</html>
+
---
+++ public/src/Pages/Main/main.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Главная</title>
+</head>
+    <body>
+        <nav class="navbar">
+            <div class="logo">logo</div>
+            <ul class="sign">
+                <div class="sign-in">sign-in</div>
+                <div class="sign-up">sign-out</div>
+            </ul>
+        </nav>
+
+        <header class="container main-header">
+            <div class="main-header-text">
+                <h1>
+                    Учите информацию при помощи сервиса <span>IntelPos</span>
+                </h1>
+                <p>
+                    Сервис, который поможет вам сделать процесс запоминания и обучения максимально эффективным
+                </p>
+                <div class="buttons" >
+                    <button id="begin" >Начать</button>
+                    <button id="more" >Подробнее о IntelPos &nbsp;
+                        <img width="20px" src="https://thumb.tildacdn.com/tild6232-6262-4831-b462-333336373038/-/format/webp/image.png">
+                    </button>
+                </div>
+            </div>
+
+
+            <img width="50%"  src="https://neumeka.ru/images/stat/int/skachat_kartinki/04.jpg" />
+
+        </header>
+                
+        <main class="container">
+            <h2> Преимущества</h2>
+        </main>
+
+        <footer class="container">
+
+        </footer>
+    </body>
+</html>
