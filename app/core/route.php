368b5cbf96bee468eeea0f1d2a52d183d0a7b27a
Supnovik <nikita_malytin@dotwrk.com>
2021-11-22 20:21:13 +0700

    change routing

--- app/core/route.php
+++ app/core/route.php
@@ -15,62 +15,61 @@ class Route
             $GLOBALS['isLogin'] = true;
             $GLOBALS['user'] = $_COOKIE['user'];
         }
-        $uri = explode('/', $_SERVER['REQUEST_URI']);
-        switch ('/' . $uri[1]) {
-            case '/':
 
-                $controller = new Controller_MainPage;
+        $path = ['' => function () {
+            $controller = new Controller_MainPage;
+            $controller->actionIndex();
+        }, 'main' => function () {
+            $controller = new Controller_MainPage;
+            $controller->actionIndex();
+        }, 'login' => function () {
+            $controller = new Controller_LoginPage;
+            $controller->actionIndex();
+        }, 'registration' => function () {
+            $controller = new Controller_RegistrationPage;
+            $controller->actionIndex();
+        }, 'list_of_users' => function () {
+            $controller = new Controller_ListOfUsersPage;
+            $controller->actionIndex();
+        }, 'users' => function () {
+            $db = new Model_Database('data', 'users');
+            $uri = explode('/', $_SERVER['REQUEST_URI']);
+            $flag = false;
+            if ($db->checkingForExistence($uri[2]) && !isset($uri[3])) {
+                $flag = true;
+                $controller = new Controller_ProfilePage;
                 $controller->actionIndex();
-                break;
-            case '/main':
-                $controller = new Controller_MainPage;
-                $controller->actionIndex();
-                break;
-            case '/login':
-                $controller = new Controller_LoginPage;
-                $controller->actionIndex();
-                break;
-            case '/registration':
-                $controller = new Controller_RegistrationPage;
-                $controller->actionIndex();
-                break;
-            case '/list_of_users':
-                $controller = new Controller_ListOfUsersPage;
-                $controller->actionIndex();
-                break;
-            case '/users':
-                $db = new Model_Database('data', 'users');
-                $uri = explode('/', $_SERVER['REQUEST_URI']);
-                $flag = false;
-                if ($db->checkingForExistence($uri[2]) && !isset($uri[3])) {
+            }
+            if ($db->checkingForExistence($uri[2]) && isset($uri[3]) && isset($uri[4])) {
+                $db = new Model_User($uri[2], $uri[2]);
+                if ($db->checkingSetofcardsForExistence($uri[4]) && $uri[3] == 'setofcards') {
                     $flag = true;
-                    $controller = new Controller_ProfilePage;
+                    $controller = new Controller_SetOfCardsPage();
+                    $controller->setData($uri[2], $uri[4]);
+                    $controller->actionIndex();
+                } elseif ($db->checkingSetofcardsForExistence($uri[4]) && $uri[3] == 'backdrops') {
+                    $flag = true;
+                    $controller = new Controller_BackdropsListPage();
+                    $controller->setData($uri[2], $uri[4]);
                     $controller->actionIndex();
                 }
-                if ($db->checkingForExistence($uri[2]) && isset($uri[3]) && isset($uri[4])) {
-                    $db = new Model_User($uri[2], $uri[2]);
-                    if ($db->checkingSetofcardsForExistence($uri[4]) && $uri[3] == 'setofcards') {
-                        $flag = true;
-                        $controller = new Controller_SetOfCardsPage();
-                        $controller->setData($uri[2], $uri[4]);
-                        $controller->actionIndex();
-                    } elseif ($db->checkingSetofcardsForExistence($uri[4]) && $uri[3] == 'backdrops') {
-                        $flag = true;
-                        $controller = new Controller_BackdropsListPage();
-                        $controller->setData($uri[2], $uri[4]);
-                        $controller->actionIndex();
-                    }
-                }
-                if (!$flag) {
-                    echo '<html><body><h1>Page Not Found</h1></body></html>';
-                    print_r($uri);
-                }
-
-                break;
-            default:
+            }
+            if (!$flag) {
                 echo '<html><body><h1>Page Not Found</h1></body></html>';
-                echo $uri[1];
-                break;
+                print_r($uri);
+            }
+        }];
+
+        $uri = explode('/', $_SERVER['REQUEST_URI']);
+
+        if (array_key_exists($uri[1], $path)) {
+            $func = $path[$uri[1]];
+            $func();
+        } else {
+            echo '<html><body><h1>Page Not Found</h1></body></html>';
+            echo $uri[1];
         }
+
     }
+
 }
