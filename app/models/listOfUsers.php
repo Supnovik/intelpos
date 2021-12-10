73427f83072db6c2b03d30766dcb95d06027bb29
Supnovik <nikita_malytin@dotwrk.com>
2021-12-10 12:39:18 +0700

    change method in list of users model

--- app/models/listOfUsers.php
+++ app/models/listOfUsers.php
@@ -9,8 +9,7 @@ class listOfUsers extends Model
 
     public function getData($user = null, $data = null)
     {
-        $db = new database('data', 'users');
-
-        return $db->getUsers();
+        $db = new dbConstructor();
+        return $db->getContent('users',['nickname']);
     }
 }
